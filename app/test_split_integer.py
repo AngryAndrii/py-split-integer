aaea39147d4c6ab1a680492a89c6fd82c32593db
Andrii Chychkan <mywork1728@gmail>
2025-11-12 09:24:33 +0200

    fixed

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -7,17 +7,17 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert (split_integer(30, 6) == [5, 5, 5, 5, 5, 5]), \
+    assert (split_integer(6, 2) == [3, 3]), \
         "parts not equal when value divisible by parts"
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert (split_integer(19, 1) == [19]), \
+    assert (split_integer(8, 1) == [8]), \
         "Need to return value, when split into one part!"
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert (split_integer(17, 4) == [4, 4, 4, 5]), \
+    assert (split_integer(32, 6) == [5, 5, 5, 5, 6, 6]), \
         "result list should be sorted!"
 
 
